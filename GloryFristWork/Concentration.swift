e37eab03f5deef5e3e75c0c14a65e0e8f0a74954
Brian LEE <hys83751952@gmail.com>
2023-10-18 17:51:58 +0900

    10.18 4강 protocol, closure, String, Optionals

--- GloryFristWork/Concentration.swift
+++ GloryFristWork/Concentration.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Concentration {
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
     var indexOfOneAndOnlyFaceUpCard : Int? {
         get {
@@ -33,6 +33,7 @@ class Concentration {
     
     
     func chooseCard(at index:Int) {
+        assert(cards.indices.contains(index), "Concentreation.chooseCard(at: \(index)): chosen index not in the cards" )
         if !cards[index].isMatched{
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 // check if cards amtch
@@ -49,6 +50,7 @@ class Concentration {
     }
     
     init(numberOfPairsOfCards : Int) {
+        assert(numberOfPairsOfCards > 0, "Concentreation.init(at: \(numberOfPairsOfCards): you need one pair of cards" )
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()
              cards += [card, card]
